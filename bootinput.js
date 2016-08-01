d736e0ddf0bdaeab576b285a33f29bc2db8ac4af
Vijay Sapkota <bvsapkota@gmail.com>
2016-08-01 19:15:38 +0200

    initial commit

---
+++ bootinput.js
@@ -0,0 +1,34 @@
+Vue.component('boot-input', {
+    template: '<div v-bind:class="{\'has-error\':error.length>0}" class="form-group"><label class="control-label">{{ label }}</label><input :type="type" class="form-control" v-model="model" :value="value" /><p class="help-block">{{ error[0] }}</p></div>',
+    props: ['model', 'type', 'value', 'error', 'label'],
+    ready: function () {
+        this.error = [];
+    }
+});
+
+
+Vue.component('boot-textarea', {
+    template: '<div v-bind:class="{\'has-error\':error.length>0}" class="form-group"><label class="control-label">{{ label }}</label><textarea class="form-control" rows="3" cols="30" v-model="model" :value="value" /><p class="help-block">{{ error[0] }}</p></div>',
+    props: ['model', 'value', 'error', 'label'],
+    ready: function () {
+        this.error = [];
+    }
+});
+
+Vue.component('boot-select', {
+    template: '<div v-bind:class="{\'has-error\':error.length>0}" class="form-group"><label class="control-label">{{ label }}</label><select class="form-control" v-model="model"><option v-for="(key,option) in options" value="{{key}}">{{option}}</option></select><p class="help-block">{{ error[0] }}</p></div>',
+    props: ['model', 'value', 'error', 'label','options'],
+    ready: function () {
+        this.error = [];
+    }
+});
+
+
+
+Vue.component('boot-checkbox', {
+    template: '<div v-bind:class="{\'has-error\':error.length>0}" class="form-group"><label><input type="checkbox" v-model="model" :value="value" /> {{ label }}</label><p class="help-block">{{ error[0] }}</p></div>',
+    props: ['model', 'value', 'error', 'label'],
+    ready: function () {
+        this.error = [];
+    }
+});
